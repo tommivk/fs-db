c73532870eebc0417d161ea0f82245551c0f2352
tommivk <tommikokk@gmail.com>
2026-05-02 20:09:29 +0300

    use sequalize.sync

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
+const { sequelize } = require("../util/db");
 const Blog = require("./blog");
 const User = require("./user");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
 
-User.sync({ alter: true });
-Blog.sync({ alter: true });
+sequelize.sync({ alter: true });
 
 module.exports = {
   Blog,
