2f2f9153dfcdee12f1376283737a30efe79b8d4c
tommivk <tommikokk@gmail.com>
2026-05-02 18:50:00 +0300

    add search functionality for blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Blog, User } = require("../models");
 const jwt = require("jsonwebtoken");
 const { SECRET } = require("../util/config");
+const { Op } = require("sequelize");
 
 const tokenExtractor = (req, res, next) => {
   const authorization = req.get("authorization");
@@ -26,12 +27,30 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
+  const where = req.query.search
+    ? {
+        [Op.or]: [
+          {
+            title: {
+              [Op.iLike]: `%${req.query.search}%`,
+            },
+          },
+          {
+            author: {
+              [Op.iLike]: `%${req.query.search}%`,
+            },
+          },
+        ],
+      }
+    : {};
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userId"] },
     include: {
       model: User,
       attributes: ["name"],
     },
+    where,
+    order: [["likes", "DESC"]],
   });
   res.json(blogs);
 });
