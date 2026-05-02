61d71217d53f4623c7743842c0598eff09784f8d
tommivk <tommikokk@gmail.com>
2026-05-02 19:09:24 +0300

    add route for author stats

---
+++ controllers/authors.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const { Blog } = require("../models");
+const { sequelize } = require("../util/db");
+
+router.get("/", async (req, res) => {
+  const blogs = await Blog.findAll({
+    attributes: [
+      "author",
+      [sequelize.fn("SUM", sequelize.col("likes")), "likes"],
+      [sequelize.fn("COUNT", sequelize.col("id")), "articles"],
+    ],
+    group: ["author"],
+    order: [["likes", "DESC"]],
+  });
+  res.json(blogs);
+});
+
+module.exports = router;
