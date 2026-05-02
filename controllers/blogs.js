a661fa3292baa1462a03be8e2120dd6301247748
tommivk <tommikokk@gmail.com>
2026-05-02 16:56:35 +0300

    add put route for updating likes

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,7 +12,6 @@ router.get("/", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    console.log({ body: req.body });
     const blog = await Blog.create({ ...req.body });
     return res.json(blog);
   } catch (error) {
@@ -28,6 +27,21 @@ router.delete("/:id", async (req, res) => {
       throw new Error(`Blog with id: ${id} not found`);
     }
     await blog.destroy();
+    return res.sendStatus(200);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { likes } = req.body;
+    const blog = await Blog.findByPk(id);
+    if (!blog) {
+      throw new Error(`Blog with id: ${id} not found`);
+    }
+    await blog.update({ likes });
     return res.sendStatus(200);
   } catch (error) {
     return res.status(400).json({ error });
